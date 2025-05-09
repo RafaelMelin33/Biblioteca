b5178e8e683d23f7543c4660beea163e7380b305
RafaelMelin33 <rafael.melinsenai@gmail.com>
2025-05-09 11:01:29 -0300

    correção dos códigos
    utilização de dicionário dentro de lista para fazer salvar um livro e criação da função devolver

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.secret_key = 'chavemuitosecretacomçparahackeramericanonaoconseguirinvadir'
 
 livros = []
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -17,42 +18,51 @@ def catalogo():
 @app.route('/adicionar_livro', methods=['GET', 'POST'])
 def adicionar_livro():
     if request.method == 'POST':
-        nome = request.form['nome']
-        autor = request.form['autor']
-        ano = request.form['ano']
-        codigo = len(livros)
-        livros.append([codigo, nome, autor, ano])
+        livros.append({
+            'nome': request.form['nome'],
+            'autor': request.form['autor'],
+            'ano': request.form['ano'],
+            'devolver_ate': "",
+            'emprestado': False
+        })
         flash('Livro cadastrado com sucesso!')
         return redirect('/catalogo')
     else:
         return render_template('adicionar_livro.html')
 
-@app.route('/editar_livro/<int:codigo>', methods=['GET', 'POST'])
-def editar_livro(codigo):
+@app.route('/editar_livro/<int:indice>', methods=['GET', 'POST'])
+def editar_livro(indice):
+
     if request.method == 'POST':
-        nome = request.form['nome']
-        autor = request.form['autor']
-        ano = request.form['ano']
-        livros[codigo] = [nome, autor, ano]
+        livros[indice]['nome'] = request.form['nome']
+        livros[indice]['autor'] = request.form['autor']
+        livros[indice]['ano'] = request.form['ano']
         flash('Livro editado com sucesso!')
         return redirect('/catalogo')
     else:
-        livro = livros[codigo]
+        livro = livros[indice]
         return render_template('editar_livro.html', livro=livro)
 
-@app.route('/apagar_livro/<int:codigo>')
-def apagar_livro(codigo):
-    del livros[codigo]
+@app.route('/apagar_livro/<int:indice>')
+def apagar_livro(indice):
+    del livros[indice]
     flash('Livro apagado com sucesso! :(')
     return redirect('/catalogo')
 
-@app.route('/emprestar_livro/<int:codigo>')
-def emprestar_livro(codigo):
-    hoje = datetime.now()
-    devolver = (hoje + timedelta(days = +7)).strftime("%d/%m/%y")
-    livros[codigo].append(devolver)
+@app.route('/emprestar_livro/<int:indice>')
+def emprestar_livro(indice):
+    if livros[indice]['emprestado'] == False:
+        hoje = datetime.now()
+        devolver = (hoje + timedelta(days = -7)).strftime("%d/%m/%y")
+        livros[indice]['devolver_ate'] = devolver
+        livros[indice]['emprestado'] = True
     return redirect('/catalogo')
 
+@app.route('/devolver_livro/<int:indice>')
+def devolver_livro(indice):
+    if livros[indice]['emprestado'] == True:
+        livros[indice]['emprestado'] = False
+    return redirect('/catalogo')
 
 if __name__ == '__main__':
     app.run(debug=True)
